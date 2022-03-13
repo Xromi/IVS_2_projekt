799b7589af1baf92a1dff9c6fd996e6125ee4944
xchrom19 <63284818+Xromi@users.noreply.github.com>
2022-03-13 16:10:41 +0100

    Added empty but named functions

--- src/math_lib.c
+++ src/math_lib.c
@@ -2,10 +2,50 @@
 #include <math.h>
 #include <stdio.h>
 
-void print_hello(){
-    printf("Hello\n");
+// Addition
+double my_add(double x, double y){
+	return x+y;
 }
 
-double my_sqrt(double x){
-    return sqrt(x);
+// Subtraction
+double my_subtract(double x, double y){
+	return x-y;
+}
+
+// Multiplication
+double my_multiply(double x, double y){
+	return x*y;
+}
+
+// Division
+double my_divide(double x, double y){
+	return x/y;
+}
+
+// Factorial
+double my_factorial(double x){
+	/*
+	int f = 1;       
+    for(int i = 1; i <= x; i++){    
+    	f = f * i;
+    }
+    return f;
+    */   
+}
+
+// Power
+double my_power(double x){
+	return pow(x,y)
+}
+
+// Root
+double my_root(double x, double y){
+	/*
+	return x;
+	*/
+}
+
+// Logarithm
+double my_log(double x){
+	return log(x);
 }
