d3abdbea5ebd26d76c4f1eaf0e7dc0beea6481f8
xchrom19 <63284818+Xromi@users.noreply.github.com>
2022-03-20 13:57:07 +0100

    Complete math_lib library tests added

--- src/test_math_lib.py
+++ src/test_math_lib.py
@@ -1,34 +1,80 @@
-import ctypes
-import sys
-import os
+import unittest
+import math_lib
+
+from math import e
+
+class TestMathLib(unittest.TestCase):
+
+	def test_my_add(self):
+		self.assertEqual(math_lib.my_add(1, 1), 2)
+		self.assertEqual(math_lib.my_add(1, -1), 0)
+		self.assertEqual(math_lib.my_add(-1, -1), -2)
+		self.assertEqual(math_lib.my_add(1, -5), -4)
+		self.assertEqual(math_lib.my_add(5, 2.5), 7.5)
+		
+	def test_my_subtract(self):
+		self.assertEqual(math_lib.my_subtract(1, 1), 0)
+		self.assertEqual(math_lib.my_subtract(1, -1), 2)
+		self.assertEqual(math_lib.my_subtract(-1, -1), 0)
+		self.assertEqual(math_lib.my_subtract(1, -5), 6)
+		self.assertEqual(math_lib.my_subtract(5, 2.5), 2.5)	
+	
+	def test_my_multiply(self):
+		self.assertEqual(math_lib.my_multiply(1, 1), 1)
+		self.assertEqual(math_lib.my_multiply(1, -1), -1)
+		self.assertEqual(math_lib.my_multiply(-1, 1), -1)
+		self.assertEqual(math_lib.my_multiply(-1, -1), 1)
+		self.assertEqual(math_lib.my_multiply(5, 2.5), 12.5)	
+		
+	def test_my_divide(self):
+		self.assertEqual(math_lib.my_divide(1, 1), 1.0)
+		self.assertEqual(math_lib.my_divide(1, -1), -1.0)
+		self.assertEqual(math_lib.my_divide(-1, 1), -1.0)
+		self.assertEqual(math_lib.my_divide(-1, -1), 1.0)
+		self.assertEqual(math_lib.my_divide(5, 2.5), 2.0)
+		
+		with self.assertRaises(ZeroDivisionError):
+			math_lib.my_divide(5, 0)
+		
+	def test_my_factorial(self):
+		self.assertEqual(math_lib.my_factorial(0), 1)
+		self.assertEqual(math_lib.my_factorial(1), 1)
+		self.assertEqual(math_lib.my_factorial(5), 120)
+		self.assertEqual(math_lib.my_factorial(10), 3628800)
+		
+		#negative factorial
+		with self.assertRaises(ValueError):
+			math_lib.my_factorial(-5)
+            
+	def test_my_power(self):
+		self.assertEqual(math_lib.my_power(1, 1), 1)
+		self.assertEqual(math_lib.my_power(1, -1), 1)
+		self.assertEqual(math_lib.my_power(-1, 1), -1)
+		self.assertEqual(math_lib.my_power(-1, -1), -1)
+		self.assertEqual(math_lib.my_power(5, 5), 3125)	
+
+	def test_my_root(self):
+		self.assertEqual(math_lib.my_root(1, 1), 1.0)
+		self.assertEqual(math_lib.my_root(1, -1), -1.0)
+		self.assertEqual(math_lib.my_root(-1, 1), 1.0)
+		self.assertEqual(math_lib.my_root(-1, -1), -1.0)
+		self.assertEqual(math_lib.my_root(2, 25), 5.0)
+		
+		#even degree root of a negative number
+		with self.assertRaises(ValueError):
+			math_lib.my_root(2, -5)
+		
+	def test_my_log(self):
+		self.assertEqual(math_lib.my_log(1), 0)
+		self.assertEqual(math_lib.my_log(e), 1)
+		
+		#natural logarithm of a negative number
+		with self.assertRaises(ValueError):
+			math_lib.my_log(-5)
+
+		#natural logarithm of zero
+		with self.assertRaises(ValueError):
+			math_lib.my_log(0)
 
 if __name__ == "__main__":
-    if (len(sys.argv) != 2):
-        print("Error: Expecting one command line argument.")
-        sys.exit()
-    
-    # Load the shared library into ctypes 
-    lib_path = os.path.abspath(os.path.join(os.path.dirname(__file__), sys.argv[1]))
-    try:
-        c_lib = ctypes.CDLL(lib_path)
-    except OSError:
-        print("Unable to load library")
-        sys.exit()
-    
-    # Testing
-    print("Testing...")
-
-    """
-    # create named function pointers and set argument and return value types
-    print_hello = c_lib.print_hello
-    print_hello.argtypes = None
-    print_hello.restype = None
-
-    my_sqrt = c_lib.my_sqrt
-    my_sqrt.argtypes = [ctypes.c_double]
-    my_sqrt.restype = ctypes.c_double
-
-    # call C functions
-    print_hello()
-    print(my_sqrt(14))
-    """
+    unittest.main()
