6a2d5b5aac5ec1c177d91e2e0bb99c6ed3c82449
SuprisinglyBoring <david.klajbl@gmail.com>
2022-03-26 12:08:03 +0100

    Implemented exception handling and fixed my_power function

--- src/math_lib.py
+++ src/math_lib.py
@@ -4,48 +4,76 @@
 # @version 0.2
 # @date 2022-03-19
 
-from math import *
+from math import factorial
 
 ##
 # @brief Addition of two numbers
 #
 # @param x First term
 # @param y Second term
+#
+# @exception TypeError Function raises TypeError when first or second term is not float or int data type
+#
 # @return Sum of x and y
 def my_add(x, y):
-	return x + y
+	if (type(x) != float and type(x) != int) or (type(y) != float and type(y) != int):
+		raise TypeError("First and second term have to be float or int numbers.")
+	else:
+		return x + y
 
 ##
 # @brief Subtraction of two numbers
 #
 # @param x First term
 # @param y Second term
+#
+# @exception TypeError Function raises TypeError when first or second term is not float or int data type
+#
 # @return Difference of x and y
 def my_subtract(x, y):
-	return x - y
+	if (type(x) != float and type(x) != int) or (type(y) != float and type(y) != int):
+		raise TypeError("First and second term have to be float or int numbers.")
+	else:
+		return x - y
 
 ##
 # @brief Multiplication of two numbers
 #
 # @param x First factor
 # @param y Second factor
+#
+# @exception TypeError Function raises TypeError when first or second factor is not float or int data type
+#
 # @return Product of x and y
 def my_multiply(x, y):
-	return x * y
+	if (type(x) != float and type(x) != int) or (type(y) != float and type(y) != int):
+		raise TypeError("First and second factor have to be float or int numbers.")
+	else:
+		return x * y
 
 ##
 # @brief Division of two numbers
 #
 # @param x Dividend
 # @param y Divisor
+#
+# @exception ZeroDivisionError Function raises ZeroDivisionError if divisor is negative value
+# @exception TypeError         Function raises TypeError when divident or divisor is not float or int data type
+#
 # @return Quotient of x and y
 def my_divide(x, y):
-	return x / y
+	if (type(x) != float and type(x) != int) or (type(y) != float and type(y) != int):
+		raise TypeError("Divident and divisor have to be float or int numbers.")
+	else:
+		return x / y
 
 ## 
 # @brief Factorial of a number
 # 
 # @param x Non-negative integer
+#
+# @exception ValueError Function raises ValueError if x is negative or none integeral value
+#
 # @return Factorial of x
 def my_factorial(x):
     return factorial(x)
@@ -55,15 +83,31 @@ def my_factorial(x):
 #
 # @param x Base
 # @param y Exponent
+#
+# @exception ValueError Function raises ValueError when base is a negative number and 1/exponent is not an odd number
+# @exception TypeError  Function raises TypeError when base or exponent is not float or int data type
+#
 # @return Value of x to the power of y
 def my_power(x, y):
-	return x ** y
+	if (type(x) != float and type(x) != int) or (type(y) != float and type(y) != int):
+		raise TypeError("Base and exponent have to be float or int numbers.")
+	elif x < 0:
+		if not (float(abs(1/y)).is_integer() and abs(1/y)%2 == 1):
+			raise ValueError("Root of negative number has to be odd integer.")
+		else:
+			return -((-x) ** (y))
+	else:
+		return x ** y
 
 ##
 # @brief N-th root of a number
 #
 # @param n Degree
 # @param x Radicand
+#
+# @exception ValueError Function raises ValueError when redicant is a negative number and degree is not an odd number
+# @exception ValueError	Function raises ValueError when base or exponent is not float or int data type
+#
 # @return Value of n-th root of x
 def my_root(n, x):
-	return x ** (1 / n)
+	return my_power(x, (1 / n))
