e7302143aaa91022bf7be2be37b65822f19a962e
SuprisinglyBoring <david.klajbl@gmail.com>
2022-04-08 23:00:45 +0200

    Added template for testing expression parsing.

--- src/exp_parser/test.py
+++ src/exp_parser/test.py
@@ -1,16 +1,35 @@
 import exp_parse
 import exp_validate
 
-expressions = open('test.txt', 'r').read().splitlines()
+print("Testing VALID expressions:")
+valid_expressions = open("test_exp_valid.txt", "r").read().splitlines()
+for exp in valid_expressions:
+    try:
+        exp_list = exp_parse.parse_expression(exp)
+    except:
+        print(f"FAIL: {exp}")
+        continue
 
-for exp in expressions:
-    exp_list = exp_parse.parse_expression(exp)
-    if not exp_list and exp:
-        print(f"Unable to parse expression string [{exp}].")
-        exit(1)
+    if exp_validate.validate_expression(exp_list) == False:
+        print(f"FAIL: {exp}")
+        continue
 
-    if not exp_validate.validate_expression(exp_list):
-        print(f"Expression [{exp}] has invalid syntax.")
-        exit(1)
+    print(f"OK: {exp}")
 
-    print(f"Expression [{exp}] is valid.")
+# separator
+print(28* "-")
+
+print("Testing INVALID expressions:")
+invalid_expressions = open("test_exp_invalid.txt", "r").read().splitlines()
+for exp in invalid_expressions:
+    try:
+        exp_list = exp_parse.parse_expression(exp)
+    except:
+        print(f"OK: {exp}")
+        continue
+
+    if exp_validate.validate_expression(exp_list) == False:
+        print(f"OK: {exp}")
+        continue
+
+    print(f"FAIL: {exp}")
