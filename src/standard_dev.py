a0b67327ddf71c003dcd5a4d56ac58c3c9b1db5d
ares <martin.horvat@protonmail.com>
2022-04-19 17:14:37 +0200

    Profiling edits.

--- src/standard_dev.py
+++ src/standard_dev.py
@@ -8,25 +8,21 @@ def std_dev():
 	numbers=[]
 	N=0
 	sum=0
+	psum=0
 
 	for word in text_input:
 		numbers.append(float(word))
-		sum=my_add(sum, numbers[-1])
+
+	
+	for number in numbers:
+		sum=my_add(number, sum);
+		psum=my_add(psum, my_power(number, 2))
 		N=my_add(N,1)
 
-
-
-	r=my_multiply(my_power(my_divide(sum, N),2),N)
-	sum=0
-
-	for number in numbers:
-		print(number)
-		number=my_subtract(my_power(number, 2), r)
-		sum=my_add(number, sum)
-
-
-	a=my_divide(1, my_subtract(N,1))
-	std_dev_value=my_root(my_multiply(a, sum),2)
-	return std_dev_value
+	
+	average=my_divide(sum, N)
+	std_dev=my_root(2,my_divide(my_subtract(psum, my_multiply(N, my_power(average, 2))), my_subtract(N, 1)))
+	return std_dev
 
 cProfile.run("std_dev()")
+
