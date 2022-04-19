092095af15b474b1fd4808e830a20a888dd42761
SuprisinglyBoring <david.klajbl@gmail.com>
2022-04-19 22:44:05 +0200

    Fixed number rounding error.

--- src/tests/exp_parser_tests/result.py
+++ src/tests/exp_parser_tests/result.py
@@ -13,10 +13,21 @@ for exp in expressions:
     if validate_expression(exp_list) == False:
         print(f"FAIL (Validation): '{values[0]}'")
         continue
+    
+    result = eval_expression(exp_list)
 
-    if eval_expression(exp_list) != values[1]:
+    try: # convert result to either float (with 8 decimals) or to int and then back to string
+        result = float(result)
+        if(result != int(result)):
+            result = round(result, 8)
+        else:
+            result = int(result)
+        result = str(result)
+    except:
+        pass
+
+    if result != values[1]:
         print(f"FAIL (Result): '{values[0]}'={values[1]}")
-        result = eval_expression(exp_list)
         print(f"Result is->{result}")
         print(f"")
         continue
