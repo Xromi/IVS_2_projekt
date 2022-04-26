b5f6146c2105f57e272bd6d8aa983cec6cd1ec8c
SuprisinglyBoring <david.klajbl@gmail.com>
2022-04-26 16:01:04 +0200

    Added setup files needed for creating debian package.

---
+++ src/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "CalculateIT",
+    version = "1.0",
+    author = "CalculateIT",
+    description = "Simple but powerfull GUI calculator.",
+    long_description="CalculateIT is high performance calculator suitable for researchers and computer science experts.",
+    license = "GPLv3",
+    url = "https://github.com/Xromi/IVS_2_projekt",
+    packages=find_packages(include=['calculateit', 'calculateit.*']),
+    entry_points = {
+        'gui_scripts' : ['calculateit = calculateit.calc:main']
+    },
+    data_files = [
+        ('share/applications/', ['calculateit.desktop']),
+        ('share/icons/', ['calculateit.xpm'])
+    ],
+    classifiers=[
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Programming Language :: Python :: 3.8",
+        "Operating System :: POSIX :: Linux"
+    ]
+)
